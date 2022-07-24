2aa8e3fb609a58b077dc4ac155a9b9353a64379b
Marc <reidymarc@googlemail.com>
2022-07-24 19:03:45 +0100

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,16 +13,14 @@ weatherForm.addEventListener("submit", (e) => {
 
     const location = search.value;
 
-    fetch("http://localhost:3000/weather?address=" + location).then(
-        (response) => {
-            response.json().then((data) => {
-                if (data.error) {
-                    messageOne.textContent = data.error;
-                } else {
-                    messageOne.textContent = data.location;
-                    messageTwo.textContent = data.forecast;
-                }
-            });
-        }
-    );
+    fetch("/weather?address=" + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });
 });
